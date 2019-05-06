c005142740fdbf815145f8e31dce4b0fa28cd7fa
Daniel <portilla.dan@gmail.com>
2019-05-06 15:13:11 -0400

    primer commit

---
+++ precache-manifest.30c6e8d6ab799bcfbb893595415cfc3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60744b320838c85468354821d34a5487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa4aebe7120620c80719",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "fa4aebe7120620c80719",
+    "url": "/static/js/main.946da490.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
